3a63ca5be71c654bb0333d11fda0fd06d3e1d9b1
Aluno Fiar Logatti <aluno@fiar.local>
2018-02-28 21:43:53 -0300

    Atualização

--- src/br/com/proj/dados/MotoDB.java
+++ src/br/com/proj/dados/MotoDB.java
@@ -1,30 +1,52 @@
 package br.com.proj.dados;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
+import com.mysql.jdbc.PreparedStatement;
+import br.com.proj.util.Conexao;
 import br.com.proj.entidade.Moto;
 
-public class MotoDB implements IMotoDB{
+public class MotoDB implements IMotoDB {
+
+	private Connection con;
+	private PreparedStatement ps;
 
 	public MotoDB() {
 		// ABERTURA DA CONEXAO COM A BASE DE DADOS
-	}
-
-	public boolean Insert(Moto moto) {
-
-		System.out.println("Id: " + moto.getId());
-		System.out.println("Descricao: " + moto.getDescricao());
-		System.out.println("Ano: " + moto.getAno());
-
-		// IMPLEMENTACAO DO INSERT NA BASE DE DADOS
-
-		return true;
+		con = Conexao.getConnection();
 	}
 
 	@Override
 	public boolean insert(Moto moto) {
 		// TODO Auto-generated method stub
-		return false;
+
+		System.out.println("Id: " + moto.getId());
+		System.out.println("Descricao: " + moto.getDescricao());
+		System.out.println("Ano: " + moto.getAno());
+
+		boolean status = false;
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("INSERT INTO MOTO");
+		sb.append(" (id, descricao, ano) values (?, ?, ?)");
+
+		try {
+			ps = this.con.PrepareStatement(sb.toString());
+
+			ps.setInt(1, moto.getId());
+			ps.setString(2, moto.getDescricao());
+			ps.setInt(3, moto.getAno());
+
+			return ps.execute();
+			
+			return true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
